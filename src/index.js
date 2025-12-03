d049cf5fe7d6b50577bb80517333adf457fc4b9d
TubbyStubby <tubbystubby2@gmail.com>
2025-12-04 01:37:43 +0530

    header button fix

--- src/index.js
+++ src/index.js
@@ -45,9 +45,15 @@ export class JsonMiller {
         this.header.innerHTML = `
             <h3>${this.title}</h3>
             <div style="display: flex; gap: 10px;">
-                <button class="jm-theme-btn">🌙</button>
-                <button class="jm-lock-btn">🔓</button>
-                <button class="jm-copy-btn">📋</button>
+                <button class="jm-theme-btn" title="Toggle Theme">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20" fill="currentColor"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Z"/></svg>
+                </button>
+                <button class="jm-lock-btn" title="Lock/Unlock">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20" fill="currentColor"><path d="M240-80q-33 0-56.5-23.5T160-160v-400q0-33 23.5-56.5T240-640h40v-80q0-83 58.5-141.5T480-920q83 0 141.5 58.5T680-720v80h80v80h-80v-160q0-50-35-85t-85-35q-50 0-85 35t-35 85v80H240v400h480v-240h80v240q0 33-23.5 56.5T720-80H240Zm240-120q33 0 56.5-23.5T560-360q0-33-23.5-56.5T480-440q-33 0-56.5 23.5T400-360q0 33 23.5 56.5T480-280Z"/></svg>
+                </button>
+                <button class="jm-copy-btn" title="Copy JSON">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20" fill="currentColor"><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"/></svg>
+                </button>
             </div>
         `;
 
@@ -132,14 +138,14 @@ export class JsonMiller {
     lock() {
         this.isLocked = true;
         this.container.classList.add('jm-locked');
-        if (this.lockBtn) this.lockBtn.innerText = '🔒';
+        if (this.lockBtn) this.lockBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20" fill="currentColor"><path d="M240-80q-33 0-56.5-23.5T160-160v-400q0-33 23.5-56.5T240-640h40v-80q0-83 58.5-141.5T480-920q83 0 141.5 58.5T680-720v80h40q33 0 56.5 23.5T800-560v400q0 33-23.5 56.5T720-80H240Zm0-80h480v-400H240v400Zm240-120q33 0 56.5-23.5T560-360q0-33-23.5-56.5T480-440q-33 0-56.5 23.5T400-360q0 33 23.5 56.5T480-280ZM360-640h240v-80q0-50-35-85t-85-35q-50 0-85 35t-35 85v80ZM240-160v-400 400Z"/></svg>';
         this.render({ preserveScroll: true });
     }
 
     unlock() {
         this.isLocked = false;
         this.container.classList.remove('jm-locked');
-        if (this.lockBtn) this.lockBtn.innerText = '🔓';
+        if (this.lockBtn) this.lockBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20" fill="currentColor"><path d="M240-80q-33 0-56.5-23.5T160-160v-400q0-33 23.5-56.5T240-640h40v-80q0-83 58.5-141.5T480-920q83 0 141.5 58.5T680-720v80h80v80h-80v-160q0-50-35-85t-85-35q-50 0-85 35t-35 85v80H240v400h480v-240h80v240q0 33-23.5 56.5T720-80H240Zm240-120q33 0 56.5-23.5T560-360q0-33-23.5-56.5T480-440q-33 0-56.5 23.5T400-360q0 33 23.5 56.5T480-280Z"/></svg>';
         this.render({ preserveScroll: true });
     }
 
@@ -182,10 +188,10 @@ export class JsonMiller {
     _updateTheme() {
         if (this.isDark) {
             document.documentElement.setAttribute('data-theme', 'dark');
-            if (this.themeBtn) this.themeBtn.innerText = '🌙';
+            if (this.themeBtn) this.themeBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20" fill="currentColor"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Z"/></svg>';
         } else {
             document.documentElement.setAttribute('data-theme', 'light');
-            if (this.themeBtn) this.themeBtn.innerText = '☀️';
+            if (this.themeBtn) this.themeBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20" fill="currentColor"><path d="M480-280q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Z"/></svg>';
         }
     }
 
