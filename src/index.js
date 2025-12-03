6659a17fd626bdab876ba19b2f9401e7981aed6e
TubbyStubby <tubbystubby2@gmail.com>
2025-12-04 02:49:19 +0530

    small ui improvements

--- src/index.js
+++ src/index.js
@@ -712,7 +712,7 @@ export class JsonMiller {
             if (isRequired) {
                 const lockIcon = document.createElement('span');
                 lockIcon.className = 'lock-icon';
-                lockIcon.innerHTML = '<svg fill="currentColor" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5 22h14a2 2 0 0 0 2-2v-9a2 2 0 0 0-2-2h-2V7A5 5 0 0 0 7 7v2H5a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2zm7-4.5a2 2 0 1 1 2-2 2 2 0 0 1-2 2zM9 9V7a3 3 0 0 1 6 0v2H9z"/></svg>';
+                lockIcon.innerHTML = '<svg fill="currentColor" width="15" height="15" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5 22h14a2 2 0 0 0 2-2v-9a2 2 0 0 0-2-2h-2V7A5 5 0 0 0 7 7v2H5a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2zm7-4.5a2 2 0 1 1 2-2 2 2 0 0 1-2 2zM9 9V7a3 3 0 0 1 6 0v2H9z"/></svg>';
                 row.appendChild(lockIcon);
             } else {
                 const deleteBtn = document.createElement('button');
