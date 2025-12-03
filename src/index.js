bb77111d60c610db5c66e79f33b332682da343a2
TubbyStubby <tubbystubby2@gmail.com>
2025-12-04 01:15:26 +0530

    bread crumbs copy button

--- src/index.js
+++ src/index.js
@@ -421,6 +421,35 @@ export class JsonMiller {
             }
             this.breadcrumbsContainer.appendChild(el);
         });
+
+        // Copy Path Button
+        if (this.selectionPath.length > 0) {
+            const copyBtn = document.createElement('button');
+            copyBtn.className = 'breadcrumb-copy-btn';
+            copyBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16" fill="currentColor"><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"/></svg>';
+            copyBtn.title = 'Copy Path';
+            copyBtn.onclick = (e) => {
+                e.stopPropagation();
+                let pathStr = '';
+                this.selectionPath.forEach((key, i) => {
+                    if (typeof key === 'number') {
+                        pathStr += `[${key}]`;
+                    } else {
+                        if (i > 0) pathStr += '.';
+                        pathStr += key;
+                    }
+                });
+                navigator.clipboard.writeText(pathStr);
+
+                // Visual feedback
+                const originalIcon = copyBtn.innerHTML;
+                copyBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 -960 960 960" width="16" fill="currentColor"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>';
+                setTimeout(() => {
+                    copyBtn.innerHTML = originalIcon;
+                }, 1500);
+            };
+            this.breadcrumbsContainer.appendChild(copyBtn);
+        }
     }
 
     renderColumn(path, dataContext, validationErrors = new Set()) {
